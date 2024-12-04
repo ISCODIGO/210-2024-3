1fcd021577c464c88ef391fe3698cd20ffba7b5a
Enrique Avila <senquevila@gmail.com>
2024-12-03 20:54:39 -0600

    agregar comentarios

--- ficheros/src/EscrituraFileWriter.java
+++ ficheros/src/EscrituraFileWriter.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 
 public class EscrituraFileWriter {
     public static void main(String[] args) throws IOException {
+        // append = false, sobrescribe el contenido del fichero
         FileWriter writerMayus = new FileWriter("abecedario.txt", false);
 
         for(int i = 65; i <= 90; i++) {
@@ -11,6 +12,7 @@ public class EscrituraFileWriter {
         }
         writerMayus.close();
 
+        // append = true permite agregar texto al final del archivo
         FileWriter writerMinus = new FileWriter("abecedario.txt", true);
         for(int i = 97; i <= 122; i++) {
             writerMinus.write(Character.toString(i));
